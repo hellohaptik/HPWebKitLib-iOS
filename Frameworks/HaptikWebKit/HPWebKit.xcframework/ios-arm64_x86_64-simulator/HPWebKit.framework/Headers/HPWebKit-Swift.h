b05ad0f43d84dc0b6111c7d2b3abad1ee5ed8ff2
Ketan <ketan@haptik.ai>
2021-11-11 14:45:45 +0530

    - update artifact

--- Frameworks/HaptikWebKit/HPWebKit.xcframework/ios-arm64_x86_64-simulator/HPWebKit.framework/Headers/HPWebKit-Swift.h
+++ Frameworks/HaptikWebKit/HPWebKit.xcframework/ios-arm64_x86_64-simulator/HPWebKit.framework/Headers/HPWebKit-Swift.h
@@ -242,6 +242,8 @@ SWIFT_CLASS("_TtC8HPWebKit15HPCustomBuilder")
 @property (nonatomic, copy) NSString * _Nonnull headerText;
 @property (nonatomic, copy) NSString * _Nonnull initializeLanguage;
 @property (nonatomic, copy) NSString * _Nonnull composerPlaceholder;
+@property (nonatomic) BOOL hidesTabBar;
+@property (nonatomic) BOOL loaderEnable;
 @property (nonatomic, copy) NSString * _Nonnull loaderMessage;
 @property (nonatomic) BOOL loaderAnimate;
 @property (nonatomic, strong) UIColor * _Nullable loaderColor;
@@ -249,6 +251,8 @@ SWIFT_CLASS("_TtC8HPWebKit15HPCustomBuilder")
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
 
+enum HPLoadingState : NSInteger;
+@class UIView;
 
 SWIFT_PROTOCOL("_TtP8HPWebKit15HPExternalEvent_")
 @protocol HPExternalEvent
@@ -256,6 +260,7 @@ SWIFT_PROTOCOL("_TtP8HPWebKit15HPExternalEvent_")
 /// \param error <code>Error</code> object explaining reason and recovery suggestion
 ///
 - (void)HPKitDidLoad;
+- (void)HPKitLoadingStateWithState:(enum HPLoadingState)state view:(UIView * _Nonnull)view;
 @end
 
 @class UIViewController;
@@ -295,6 +300,12 @@ SWIFT_CLASS_PROPERTY(@property (nonatomic, class, readonly, strong) HPKit * _Non
 - (void)clearConversation;
 - (void)details;
 @end
+
+typedef SWIFT_ENUM(NSInteger, HPLoadingState, open) {
+  HPLoadingStateStart = 0,
+  HPLoadingStateComplete = 1,
+  HPLoadingStateFailed = 2,
+};
 
 @class UIEvent;
 @class UITouch;
@@ -568,6 +579,8 @@ SWIFT_CLASS("_TtC8HPWebKit15HPCustomBuilder")
 @property (nonatomic, copy) NSString * _Nonnull headerText;
 @property (nonatomic, copy) NSString * _Nonnull initializeLanguage;
 @property (nonatomic, copy) NSString * _Nonnull composerPlaceholder;
+@property (nonatomic) BOOL hidesTabBar;
+@property (nonatomic) BOOL loaderEnable;
 @property (nonatomic, copy) NSString * _Nonnull loaderMessage;
 @property (nonatomic) BOOL loaderAnimate;
 @property (nonatomic, strong) UIColor * _Nullable loaderColor;
@@ -575,6 +588,8 @@ SWIFT_CLASS("_TtC8HPWebKit15HPCustomBuilder")
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
 
+enum HPLoadingState : NSInteger;
+@class UIView;
 
 SWIFT_PROTOCOL("_TtP8HPWebKit15HPExternalEvent_")
 @protocol HPExternalEvent
@@ -582,6 +597,7 @@ SWIFT_PROTOCOL("_TtP8HPWebKit15HPExternalEvent_")
 /// \param error <code>Error</code> object explaining reason and recovery suggestion
 ///
 - (void)HPKitDidLoad;
+- (void)HPKitLoadingStateWithState:(enum HPLoadingState)state view:(UIView * _Nonnull)view;
 @end
 
 @class UIViewController;
@@ -621,6 +637,12 @@ SWIFT_CLASS_PROPERTY(@property (nonatomic, class, readonly, strong) HPKit * _Non
 - (void)clearConversation;
 - (void)details;
 @end
+
+typedef SWIFT_ENUM(NSInteger, HPLoadingState, open) {
+  HPLoadingStateStart = 0,
+  HPLoadingStateComplete = 1,
+  HPLoadingStateFailed = 2,
+};
 
 @class UIEvent;
 @class UITouch;
